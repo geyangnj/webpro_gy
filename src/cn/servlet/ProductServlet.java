a31e3337143f369225e2dbf8ad9f67b11c1cd593
GeYang <geyangnj@wo.cn>
2019-04-30 12:20:57 +0800

    day-3

--- src/cn/servlet/ProductServlet.java
+++ src/cn/servlet/ProductServlet.java
@@ -4,20 +4,34 @@ import cn.cn.service.ProductService;
 import cn.model.Product;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ProductServlet extends javax.servlet.http.HttpServlet {
 
     private ProductServlet productService= new ProductService();
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+request.setCharacterEncoding(("UTF-8"));
+response.setCharacterEncoding("UTF-8");
 
         Product product =new Product();
         product.setName(request.getParameter("name"));
         product.setRemark(request);
         productService.save(product);
+ response.sendRedirect("/webpro_gy/search.jsp");
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.setCharacterEncoding(("UTF-8"));
+        response.setCharacterEncoding("UTF-8");
+
+        String keyword =request.getParameter("keyword));
+                ArrayList<Product> proList= productService(keyword));
+        ArrayList
 
     }
+
+
+
+
 }
---
+++ web/search.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: GeYang
+  Date: 2019-4-29
+  Time: 11:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$标题，哈哈哈$</title>
+  </head>
+  hello,huawei@
+  1
+  <body>
+  <form action="/webpro_gy/abc" method="get">
+    查询关键字：<input type=""text" name="keyword"/><br />
+        <button type="submit">给我搜</button>>
+  </form>
+  </body>
+</html>
